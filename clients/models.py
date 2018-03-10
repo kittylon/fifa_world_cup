b727440c34d76699ce805ca2f2d52be51aa53d03
Carolina <carolin861@gmail.com>
2018-03-10 18:45:22 -0500

    first commit

---
+++ clients/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+class Client(models.Model):
+    nit = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    profile = models.CharField(max_length=2)
+    allowed_participants = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    document_type = models.CharField(max_length=50, default="CC")
+    document_number = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=100)
+    company = models.ForeignKey(
+        'clients.Client',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name = 'users'
+    )
+    job_title = models.CharField(max_length=200)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
