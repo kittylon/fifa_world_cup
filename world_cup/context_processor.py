c3580f4b83db143db8221d1eb57ecb75c34d6356
Carolina <carolin861@gmail.com>
2018-03-20 13:13:17 -0500

    score savings for usermatches

--- world_cup/context_processor.py
+++ world_cup/context_processor.py
@@ -1,10 +1,10 @@
 from django.urls import reverse
-from world_cup.models import RealMatch, Team
+from world_cup.models import RealMatch, UserMatch, Team
 
 def Teams_data(request):
     teams = Team.objects.all()
     return {'teams': teams}
 
-def UserMatch_data(request):
-    matches = UserMatch.objects.filer(request.user)
-    return {'user_matches': user_matches}
+# def UserMatch_data(request):
+#     user_matches = UserMatch.objects.filter(user=request.user)
+#     return {'user_matches': user_matches}
--- world_cup/views.py
+++ world_cup/views.py
@@ -10,68 +10,55 @@ import json
 
 class GroupDetailView(TemplateView):
     template_name='world_cup/group_detail.html'
-    model = Team
-
-    @staticmethod
-    def check_guesses(group, user):
-        guesses =  UserGuess.objects.filter(user=user, group=group)
-        if len(guesses) > 1:
-            return guesses
-        else:
-            return False
+    model = UserMatch
 
     def get(self, request, *args, **kwargs):
         group = self.kwargs['query']
-        guesses = GroupDetailView.check_guesses(group, request.user)
-        if guesses:
-            print(guesses)
-            return render(request, self.template_name, {'guesses': guesses} )
-        object_list =  Team.objects.filter(group=group)
-        matches = []
-        for team in object_list:
-            matches += Match.objects.filter(team_one=team)
-        return render(request, self.template_name, {'matches': matches, 'object_list': object_list} )
+        object_list = UserMatch.objects.filter(group=group)
+        return render(request, self.template_name, {'object_list': object_list} )
 
 
 class GroupsView(TemplateView):
     template_name = 'world_cup/groups_phase.html'
-    # model = UserMatch
-    #
-    # @staticmethod
-    # def save_gamble(user, match, team_one, team_two):
-    #     obj_match = get_object_or_404(Match, pk=match)
-    #     gamble = UserGuess(match=obj_match, team_one_score=team_one,
-    #                     team_two_score=team_two, user=user)
-    #     gamble.save()
-    #     return gamble.pk
-    #
-    # def post(self, request, *args, **kwargs):
-    #     dict_gamble = dict(request.POST.items())
-    #     dict_gamble.pop('csrfmiddlewaretoken', None)
-    #     user = request.user
-    #     match_id = ''
-    #     team_label = ''
-    #     team_one = ''
-    #     team_two = ''
-    #     print(dict_gamble)
-    #     for key, value in dict_gamble.items():
-    #         match_id = key[0]
-    #         team_label = key[-3:]
-    #         if team_label == 'one':
-    #             team_one = value
-    #         elif team_label =='two':
-    #             team_two = value
-    #
-    #         if team_one != '' and team_two != '':
-    #             GroupsView.save_gamble(user, match_id, team_one, team_two)
-    #             team_one = ''
-    #             team_two = ''
-    #
-    #     return redirect('summary')
+    model = UserMatch
+
+    @staticmethod
+    def save_gamble(user, match, team_one, team_two):
+        user_match = get_object_or_404(UserMatch, pk=match)
+        user_match.team_one_score = team_one
+        user_match.team_two_score = team_two
+        user_match.user = user
+        user_match.gambled = True
+        user_match.save()
+        return user_match.pk
+
+    def post(self, request, *args, **kwargs):
+        dict_gamble = dict(request.POST.items())
+        dict_gamble.pop('csrfmiddlewaretoken', None)
+        user = request.user
+        match_id = ''
+        team_label = ''
+        team_one = ''
+        team_two = ''
+        print(dict_gamble)
+        for key, value in dict_gamble.items():
+            match_id = key[0]
+            team_label = key[-3:]
+            if team_label == 'one':
+                team_one = value
+            elif team_label =='two':
+                team_two = value
+
+            if team_one != '' and team_two != '':
+                GroupsView.save_gamble(user, match_id, team_one, team_two)
+                team_one = ''
+                team_two = ''
+
+        return redirect('summary')
 
 class SummaryView(TemplateView):
     template_name = 'world_cup/summary.html'
-    # 
-    # def get(self, request, *args, **kwargs):
-    #     object_list =  UserGuess.objects.filter(user=request.user)
-    #     return render(request, self.template_name, {'object_list': object_list} )
+
+    def get(self, request, *args, **kwargs):
+        object_list =  UserMatch.objects.filter(user=request.user, gambled=True)
+        return render(request, self.template_name, {'object_list': object_list} )
